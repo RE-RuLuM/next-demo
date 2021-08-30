fbd3edb078a44b256b7c84a3a879947f05d77092
Edgar Moreano Rodriguez <edgar12moreano@hotmail.com>
2021-08-30 08:37:39 -0500

    next-seo npm package for seo i in page firs-post

--- pages/_app.js
+++ pages/_app.js
@@ -5,39 +5,39 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo 
-              title="Mi aplicación Next"
-              description="Una descripcion de"
-              openGraph={{
-                url: 'https://demo-next-test.netlify.app/',
-                title: 'Mi aplicación Next',
-                description: 'Mi descripción',
-                images: [
-                  {
-                    url: 'https://placeimg.com/1200/630/any',
-                    width: 1200,
-                    height: 600,
-                    alt: 'dummy image'
-                  },
-                  {
-                    url: 'https://placeimg.com/600/300/any',
-                    width: 600,
-                    height: 300,
-                    alt: 'dummy image 2'
-                  },
-                  {
-                    url: 'https://placeimg.com/400/400/any',
-                    width: 400,
-                    height: 400,
-                    alt: 'dummy image 3'
-                  },
-                ],
-                site_name: 'Mi aplicación Next'
-              }}
-              twitter={{
-                handle: '@edgar',
-                site: '@edgar_moreano',
-                cardType: 'summary_large_image',
-              }}
+        title="Mi aplicación Next"
+        description="Una descripcion de"
+        openGraph={{
+          url: 'https://demo-next-test.netlify.app/',
+          title: 'Mi aplicación Next',
+          description: 'Mi descripción',
+          images: [
+            {
+              url: 'https://placeimg.com/1200/630/people',
+              width: 1200,
+              height: 600,
+              alt: 'dummy image'
+            },
+            {
+              url: 'https://placeimg.com/600/300/people',
+              width: 600,
+              height: 300,
+              alt: 'dummy image 2'
+            },
+            {
+              url: 'https://placeimg.com/400/400/people',
+              width: 400,
+              height: 400,
+              alt: 'dummy image 3'
+            },
+          ],
+          site_name: 'Mi aplicación Next'
+        }}
+        twitter={{
+          handle: '@edgar',
+          site: '@edgar_moreano',
+          cardType: 'summary_large_image',
+        }}
       />
       <Component {...pageProps} />
     </>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,47 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/layout'
+import { NextSeo } from 'next-seo'
 
 
 export default function FirstPost() {
     return (
       <Layout>
-        <Head>
-          <title>First Post</title>
-        </Head>
+        <NextSeo 
+        title="Mi primer post"
+        description="Una descripcion de mi post"
+        openGraph={{
+          url: 'https://demo-next-test.netlify.app/',
+          title: 'Mi primer post vista el pacifico',
+          description: 'Mi descripción de mi post og',
+          images: [
+            {
+              url: 'https://placeimg.com/1200/630/animals',
+              width: 1200,
+              height: 600,
+              alt: 'dummy image'
+            },
+            {
+              url: 'https://placeimg.com/600/300/animals',
+              width: 600,
+              height: 300,
+              alt: 'dummy image 2'
+            },
+            {
+              url: 'https://placeimg.com/400/400/animals',
+              width: 400,
+              height: 400,
+              alt: 'dummy image 3'
+            },
+          ],
+          site_name: 'Mi aplicación Next'
+        }}
+        twitter={{
+          handle: '@edgar',
+          site: '@edgar_moreano',
+          cardType: 'summary_large_image',
+        }}
+      />
         <h1>First Post</h1>
         <h2>
           <Link href="/">
